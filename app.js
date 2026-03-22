241b4a46d7e84f75b04400483279af1b322008f4
Rubens Brandão <rubensbrandao@gmail.com>
2026-03-22 13:27:13 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+
 const feed = document.getElementById("feed");
 
-const data = [
-  {title:"Mario vs Sonic", a:59, b:41},
-  {title:"iPhone vs Android", a:52, b:48}
-];
+if(feed){
+  const battles = [
+    {title:"Mario vs Sonic", a:59, b:41},
+    {title:"BTC vs ETH", a:52, b:48},
+    {title:"iPhone vs Android", a:60, b:40}
+  ];
 
-data.forEach(d=>{
-  const el = document.createElement("div");
-  el.className="card";
-  el.innerHTML=`
-    <h3>${d.title}</h3>
-    <div class="vote">
-      <button class="btnA">${d.a}%</button>
-      <button class="btnB">${d.b}%</button>
-    </div>
-  `;
-  feed.appendChild(el);
-});
+  battles.forEach(b=>{
+    const el=document.createElement("div");
+    el.className="card";
+    el.innerHTML=`
+      <h3>${b.title}</h3>
+      <div class="vote">
+        <button class="btnA">${b.a}%</button>
+        <button class="btnB">${b.b}%</button>
+      </div>
+    `;
+    feed.appendChild(el);
+  });
+}
